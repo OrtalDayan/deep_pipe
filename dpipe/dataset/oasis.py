a47f792cf64caa07370a01add8bafd6da1874efb
gleb <gleb.makarchuk@gmail.com>
2017-10-31 15:37:01 +0300

    Resize sources as well

--- dpipe/dataset/oasis.py
+++ dpipe/dataset/oasis.py
@@ -11,12 +11,17 @@ class Oasis(FromCSVInt):
             metadata_rpath=metadata_rpath,
             modalities=['S'],
             target='T',
-            segm2msegm_matrix=np.array(np.diag([0, 1, 1, 1]), dtype=bool)
+            segm2msegm_matrix=np.diag([0, 1, 1, 1])
         )
 
     def load_segm(self, patient_id):
         img = super().load_segm(patient_id).astype(int)
         # The loaded image has a shape of (176, 208, 176, 1). Next line remove the last useless dimension.
         img.resize(img.shape[:-1])
-        return super().load_segm(patient_id).astype(int)
+        return img
 
+    def load_mscan(self, patient_id):
+        mscan = super().load_mscan(patient_id)
+        for scan in mscan:
+            scan.resize(scan.shape[:-1])
+        return mscan
