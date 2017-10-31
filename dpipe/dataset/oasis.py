7584db9aca7ec98ee6e81d8ccf7c47b51a23fa80
gleb <gleb.makarchuk@gmail.com>
2017-10-31 14:55:18 +0300

    remove useless dimension from the image

--- dpipe/dataset/oasis.py
+++ dpipe/dataset/oasis.py
@@ -11,9 +11,12 @@ class Oasis(FromCSVInt):
             metadata_rpath=metadata_rpath,
             modalities=['S'],
             target='T',
-            segm2msegm_matrix=np.array(np.diag([1, 1, 1, 1]), dtype=bool)
+            segm2msegm_matrix=np.array(np.diag([0, 1, 1, 1]), dtype=bool)
         )
 
     def load_segm(self, patient_id):
+        img = super().load_segm(patient_id).astype(int)
+        # The loaded image has a shape of (176, 208, 176, 1). Next line remove the last useless dimension.
+        img.resize(img.shape[:-1])
         return super().load_segm(patient_id).astype(int)
 
