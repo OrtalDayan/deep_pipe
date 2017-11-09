4b59a36a115cdac1a933e4073c2d5e506db6a95e
gleb <gleb.makarchuk@gmail.com>
2017-11-09 14:35:13 +0300

    Compute dices for train

--- dpipe/train/train_segm.py
+++ dpipe/train/train_segm.py
@@ -13,22 +13,46 @@ from .logging import make_log_vector
 from .utils import make_find_next_lr, make_check_loss_decrease
 
 
-@register()
-def train_segm(model: Model, train_batch_iter_factory: BatchIterFactory, batch_predict: BatchPredict, log_path, val_ids,
-               dataset, *, n_epochs, lr_init, lr_dec_mul=0.5, patience: int, rtol=0, atol=0):
-    logger = Logger(log_path)
+class LoggingHelper(object):
+    def __init__(self, ids, sample_name, dataset, logger, batch_predict, model):
+        self.mscans = [dataset.load_mscan(p) for p in ids]
+        self.segms = [dataset.load_segm(p) for p in ids]
+        self.msegms = [dataset.load_msegm(p) for p in ids]
+        self.avg_log_write = logger.make_log_scalar('avg_{}_loss'.format(sample_name))
+        self.dices_log_write = make_log_vector(logger, '{}_dices'.format(sample_name))
+        self.batch_predict = batch_predict
+        self.model = model
+        self.dataset = dataset
 
-    mscans_val = [dataset.load_mscan(p) for p in val_ids]
-    segms_val = [dataset.load_segm(p) for p in val_ids]
-    msegms_val = [dataset.load_msegm(p) for p in val_ids]
+    def make_record(self):
+        msegms_pred = []
+        losses = []
+
+        for x, y in zip(self.mscans, self.segms):
+            y_pred, loss = self.batch_predict.validate(x, y, validate_fn=self.model.do_val_step)
+            msegms_pred.append(self.dataset.segm2msegm(np.argmax(y_pred, axis=0)))
+            losses.append(loss)
+
+        avg_loss = np.mean(losses)
+        self.avg_log_write(avg_loss)
+        dices = [multichannel_dice_score(pred, true) for pred, true in zip(msegms_pred, self.msegms)]
+        avg_dice = np.mean(dices, axis=0)
+        self.dices_log_write(avg_dice)
+        return avg_loss, avg_dice
+
+@register()
+def train_segm(model: Model, train_batch_iter_factory: BatchIterFactory, batch_predict: BatchPredict, log_path,
+               train_ids, val_ids, dataset, *, n_epochs, lr_init, lr_dec_mul=0.5, patience: int, rtol=0, atol=0):
+    logger = Logger(log_path)
 
     find_next_lr = make_find_next_lr(lr_init, lambda lr: lr * lr_dec_mul,
                                      partial(make_check_loss_decrease, patience=patience, rtol=rtol, atol=atol))
 
-    train_log_write = logger.make_log_scalar('train_loss')
-    train_avg_log_write = logger.make_log_scalar('avg_train_loss')
-    val_avg_log_write = logger.make_log_scalar('avg_val_loss')
-    val_dices_log_write = make_log_vector(logger, 'val_dices')
+    train_log_write = logger.make_log_scalar('train_loss_old')
+    train_avg_log_write = logger.make_log_scalar('avg_train_loss_old')
+
+    val_logging = LoggingHelper(val_ids, "val", dataset, logger, batch_predict, model)
+    train_logging = LoggingHelper(train_ids, "train", dataset, logger, batch_predict, model)
 
     lr = find_next_lr(math.inf)
     with train_batch_iter_factory, logger:
@@ -40,16 +64,7 @@ def train_segm(model: Model, train_batch_iter_factory: BatchIterFactory, batch_p
                     train_log_write(train_losses[-1])
                 train_avg_log_write(np.mean(train_losses))
 
-            msegms_pred = []
-            val_losses = []
-            for x, y in zip(mscans_val, segms_val):
-                y_pred, loss = batch_predict.validate(x, y, validate_fn=model.do_val_step)
-                msegms_pred.append(dataset.segm2msegm(np.argmax(y_pred, axis=0)))
-                val_losses.append(loss)
-
-            val_loss = np.mean(val_losses)
-            val_avg_log_write(val_loss)
-            val_dices = [multichannel_dice_score(pred, true) for pred, true in zip(msegms_pred, msegms_val)]
-            val_dices_log_write(np.mean(val_dices, axis=0))
+            train_logging.make_record()
+            val_loss, _ = val_logging.make_record()
 
             lr = find_next_lr(val_loss)
