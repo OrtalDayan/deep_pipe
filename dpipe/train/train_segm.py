8869223e6452336e7e2905e822cbda55603a6581
gleb <gleb.makarchuk@gmail.com>
2017-11-16 00:12:31 +0300

    Add some debug printing

--- dpipe/train/train_segm.py
+++ dpipe/train/train_segm.py
@@ -12,6 +12,7 @@ from dpipe.medim.metrics import multichannel_dice_score
 from .logging import make_log_vector
 from .utils import make_find_next_lr, make_check_loss_decrease
 
+import time
 
 class SampleStatsLogger:
     def __init__(self, sample_name, ids, logger, dataset, validate_fn):
@@ -32,6 +33,7 @@ class SampleStatsLogger:
             msegm_pred = self.dataset.segm2msegm(np.argmax(np.argmax(y_pred, axis=0)))
             losses.append(loss)
             dices.append(multichannel_dice_score(msegm_pred, msegm))
+        print("glebgleb: in a sample stats logger. Making a record.")
         self.avg_log_write(np.mean(losses))
         self.dices_log_write(np.mean(dices, axis=0))
 
@@ -59,13 +61,18 @@ def train_segm(model: Model, train_batch_iter_factory: BatchIterFactory, batch_p
     lr = find_next_lr(math.inf)
     with train_batch_iter_factory, logger:
         for i in range(n_epochs):
+            print("glebgleb epoch = {}".format(i))
+            st_time = time.time()
             with next(train_batch_iter_factory) as train_batch_iter:
                 train_losses = []
                 for inputs in train_batch_iter:
                     train_losses.append(model.do_train_step(*inputs, lr=lr))
                     train_log_write(train_losses[-1])
                 train_avg_log_write(np.mean(train_losses))
+            print("glebgleb spent {} s training on a batch".format(time.time() - st_time))
 
+            print("glebgleb moving to making val records in an old fashion")
+            st_time = time.time()
             msegms_pred = []
             val_losses = []
             for x, y in zip(mscans_val, segms_val):
@@ -77,9 +84,17 @@ def train_segm(model: Model, train_batch_iter_factory: BatchIterFactory, batch_p
             val_avg_log_write(val_loss)
             val_dices = [multichannel_dice_score(pred, true) for pred, true in zip(msegms_pred, msegms_val)]
             val_dices_log_write(np.mean(val_dices, axis=0))
+            print("glebgleb spent {} s making on making val records in an old fashion".format(time.time() - st_time))
 
+            print("glebgleb moving to making a val record in a new fashion")
+            st_time = time.time()
             val_logger.make_record()
+            print("glebgleb spent {} s on making a val record in a new fashion".format(time.time() - st_time))
+
             if train_logger:
+                print("glebgleb moving to making a train record in a new fashion")
+                st_time = time.time()
                 train_logger.make_record()
+                print("glebgleb spent {} s on making a train record in a new fashion".format(time.time() - st_time))
 
             lr = find_next_lr(val_loss)
